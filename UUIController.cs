22bf75bb34590ddd78da27ed60cfbe51524c0d7c
sukggom <sukggom@naver.com>
2025-10-27 11:57:01 +0900

    Update UUIController.cs

--- UUIController.cs
+++ UUIController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace UG.Framework
 {
@@ -126,3 +125,4 @@ namespace UG.Framework
         }
     }
 }
+
